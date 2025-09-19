5f69537ae2e587b2238ef2cc9fa39b356f2a926c
Govind <govindsys1008@gmail.com>
2025-09-19 17:53:16 +0530

    Added authentication APIs

---
+++ backend/backapp/api.py
@@ -0,0 +1,54 @@
+from ninja import NinjaAPI
+from .schema import LoginSchema, SignupSchema, TokenSchema
+from django.contrib.auth import authenticate
+from .models import SpashtUser
+from rest_framework_simplejwt.tokens import RefreshToken
+from ninja.errors import ValidationError
+from rest_framework_simplejwt.views import TokenRefreshView
+
+api = NinjaAPI(title="Spasht API Docs")
+
+
+@api.post("/signup", response=TokenSchema)
+def signup(request, data: SignupSchema):
+    if SpashtUser.objects.filter(username=data.username).exists():
+        raise ValidationError([{"loc": ["username"], "msg": "Username already exists"}])
+    if SpashtUser.objects.filter(email=data.email).exists():
+        raise ValidationError([{"loc": ["email"], "msg": "Email already exists"}])
+
+    if data.password1 != data.password2:
+        return api.create_response(
+            request, {"error": "Passwords don't match"}, status=400
+        )
+
+    user = SpashtUser.objects.create_user(
+        username=data.username,
+        email=data.email,
+        password=data.password1,
+        place=data.place,
+        date_of_birth=data.date_of_birth,
+        profession=data.profession,
+        mobile_no=data.mobile_no,
+    )
+    user.save()
+
+    refresh = RefreshToken.for_user(user)
+    return {"access": str(refresh.access_token), "refresh": str(refresh)}
+
+
+@api.post("/refresh")
+def refresh(request):
+    view = TokenRefreshView.as_view()
+    return view(request._request)
+
+
+@api.post("/login", response=TokenSchema)
+def login(request, data: LoginSchema):
+    user = authenticate(username=data.username, password=data.password)
+    if user is None:
+        return api.create_response(
+            request, {"error": "Invalid credentials"}, status=401
+        )
+
+    refresh = RefreshToken.for_user(user)
+    return {"access": str(refresh.access_token), "refresh": str(refresh)}
---
+++ backend/backapp/schema.py
@@ -0,0 +1,27 @@
+from ninja import Schema
+from datetime import date
+from typing import Optional
+from enum import Enum
+
+class ProfessionEnum(str, Enum):
+    student = "student"
+    law_practitioner = "law_practitioner"
+    other = "other"
+
+class LoginSchema(Schema):
+    username :str
+    password:str
+
+class TokenSchema(Schema):
+    access: str
+    refresh : str
+
+class SignupSchema(Schema):
+    username : str
+    password1 : str
+    password2 : str
+    email : str
+    place : Optional[str] = None
+    date_of_birth : Optional[date] = None
+    profession : Optional[ProfessionEnum] = None
+    mobile_no : Optional[str] = None
